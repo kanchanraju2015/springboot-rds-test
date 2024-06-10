d6417d2d5ea159b3f7b5eb2f163b5828464fb459
kanchan raju <kanchanraju2015@gmail.com>
2024-06-10 14:52:03 +0530

    this is rds first commit

---
+++ src/main/java/com/briz/springbootrdstest/SpringbootRdsTestApplication.java
@@ -0,0 +1,13 @@
+package com.briz.springbootrdstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRdsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRdsTestApplication.class, args);
+	}
+
+}
